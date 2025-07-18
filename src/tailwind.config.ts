f0341d0a2be40a61b216858702ddb960df49739c
indoterakot indoterakot <terakot2025@gmail.com>
2025-07-18 22:32:15 +0000

    почему текст и кнрпки сбоку и кнопки не доработаны исправь.

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -12,7 +12,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['Space Grotesk', 'sans-serif'],
+        headline: ['"Space Grotesk"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
